b6b8d15c8f80e78dddfbe1587e276be442bc5793
Hendrik Maus <hendrik.maus@trivago.com>
2017-10-24 08:45:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# nomad-cli
+Docker image with [Nomad](https://github.com/hashicorp/nomad) inside of it to be used as CLI tool.
+
+## Usage
+
+```bash
+docker run \
+    -v ${PWD}/service.nomad:/service.nomad \
+    hendrikmaus/nomad-cli \
+    nomad run \
+    -address=http://your-nomad:4646 \
+    service.nomad
+```
+
+## Build
+
+```bash
+./build.sh -version 0.6.3 -sha256 908ee049bda380dc931be2c8dc905e41b58e59f68715dce896d69417381b1f4e
+```
---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+for arg in "$@" ; do
+    case "$arg" in
+      -version)
+        nomad_version=$2
+        shift
+        ;;
+      -sha256)
+        nomad_sha256=$3
+        shift
+        ;;
+     esac
+done
+
+if [[ -z $nomad_version ]]; then
+    echo "Please provide version using -version"
+    echo "E.g. 0.6.3 for downloading https://releases.hashicorp.com/nomad/0.6.3/"
+    echo "All releases can be found on https://releases.hashicorp.com/nomad/"
+    exit 1
+fi
+
+if [[ -z $nomad_sha256 ]]; then
+    echo "Please provide SHA256 checksum for linux amd64 using -sha256"
+    echo "E.g. 908ee049bda380dc931be2c8dc905e41b58e59f68715dce896d69417381b1f4e for 0.6.3"
+    echo "Checksum file can be found alongside the releases, e.g. https://releases.hashicorp.com/nomad/0.6.3/"
+    exit 1
+fi
+
+NOMAD_VERSION=0.7.0-rc1
+NOMAD_SHA256=c772187901de20075a20de473e87299358dd6dbf451fc2d45ebe93533c3e6158
+docker build \
+    --build-arg arg_nomad_version=$nomad_version \
+    --build-arg arg_nomad_sha256=$nomad_sha256 \
+    -t hendrikmaus/nomad-cli:$nomad_version \
+    .
